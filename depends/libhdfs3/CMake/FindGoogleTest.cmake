7cb3b8028fcb6bde2a2aff28434a95a6610e2052
Ahmed Riza <dr.riza@gmail.com>
2022-02-12 21:46:57 +0000

    Fix build issues

--- depends/libhdfs3/CMake/FindGoogleTest.cmake
+++ depends/libhdfs3/CMake/FindGoogleTest.cmake
@@ -3,6 +3,7 @@ include(CheckCXXSourceRuns)
 find_path(GTest_INCLUDE_DIR gtest/gtest.h
           NO_DEFAULT_PATH
           PATHS
+          "/usr/include"
           "/opt/dependency/package/include"
           "${PROJECT_SOURCE_DIR}/../thirdparty/googletest/googletest/include"
           "/usr/local/include"
@@ -11,22 +12,25 @@ find_path(GTest_INCLUDE_DIR gtest/gtest.h
 find_path(GMock_INCLUDE_DIR gmock/gmock.h
           NO_DEFAULT_PATH
           PATHS
+          "/usr/include"
           "/opt/dependency/package/include"
           "${PROJECT_SOURCE_DIR}/../thirdparty/googletest/googlemock/include"
           "/usr/local/include"
           "/usr/include")
 
 find_library(Gtest_LIBRARY
-             NAMES libgtest.a
+             NAMES libgtest.so
              HINTS
+             "/usr/lib64"
              "/opt/dependency/package/lib"
              "${PROJECT_SOURCE_DIR}/../thirdparty/googletest/build/googlemock/gtest"
              "/usr/local/lib"
              "/usr/lib")
 
 find_library(Gmock_LIBRARY
-             NAMES libgmock.a
+             NAMES libgmock.so
              HINTS
+             "/usr/lib64"
              "/opt/dependency/package/lib"
              "${PROJECT_SOURCE_DIR}/../thirdparty/googletest/build/googlemock"
              "/usr/local/lib"
@@ -41,7 +45,7 @@ set(CMAKE_REQUIRED_INCLUDES ${GTest_INCLUDE_DIR} ${GMock_INCLUDE_DIR})
 set(CMAKE_REQUIRED_LIBRARIES ${Gtest_LIBRARY} ${Gmock_LIBRARY})
 set(CMAKE_REQUIRED_FLAGS "-lpthread")
 IF(OS_LINUX)
-    SET(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} -lc++")
+    SET(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} -lgtest -lgmock")
 ENDIF(OS_LINUX)
 
 check_cxx_source_runs("
